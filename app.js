b37cf3faf5d03eda53844be47f6c77cd4d950c5f
c1324737 <c1324737@interno.bb.com.br>
2022-07-26 14:40:33 -0300

    feat: Cria metodo que escuta clique do botão

--- app.js
+++ app.js
@@ -2,12 +2,31 @@
   'use strict';
 
   angular
-    .module('NarrowItDownAppController', [])
+    .module('NarrowItDownApp', [])
+    .controller('NarrowItDownAppController', NarrowItDownAppController)
+  //  .service('MenuSearchService', MenuSearchService)
 
   // Controller NarrowItDownAppController
-  function NarrowItDownAppController() {
+  NarrowItDownAppController.$inject = ['$scope'];
+
+  function NarrowItDownAppController($scope) {
     this.foundItems = [];
-    
+
+    $scope.search = function() {
+      console.log("veio")
+       NarrowItDownAppController.getMatchedMenuItems(searchTerm)
+    }
+  }
+
+  // Service MenuSearchService
+  
+  function MenuSearchService() {
+    this.getMatchedMenuItems = function (searchTerm) {
+      return $http("https://davids-restaurant.herokuapp.com/menu_items.json")
+        .then(function (result) {
+          console.log(result)
+        });
+    }
   }
 }
 )()
