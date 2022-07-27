9a9d5f4f8bd141a4ec1fe0f589a8b824f07ca731
c1324737 <c1324737@interno.bb.com.br>
2022-07-27 14:09:54 -0300

    feat: cria diretiva

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
     .module('NarrowItDownApp', [])
     .controller('NarrowItDownController', NarrowItDownController)
     .service('MenuSearchService', MenuSearchService)
+    .directive('foundItems', FoundItemsDirective)
 
       
   // Service MenuSearchService  
@@ -25,14 +26,78 @@
   }
   
 
+
+  // FoundItemsDirective
+  function FoundItemsDirective() {
+    var ddo = {
+      templateUrl: 'foundItems.html',
+      scope: { found: '<' },
+      controller: FoundItemsDirectiveController,
+      controllerAs: 'list',
+      bindToController: true,
+      link: FoundItemsDirectiveLink
+    };
+
+    return ddo;
+  }
+
+  function FoundItemsDirectiveLink(scope, element, attrs, controller) {
+    console.log("Link scope is: ", scope);
+    console.log("Controller instance is: ", controller);
+    console.log("Element is: ", element);
+
+    scope.$watch('list.cookiesInList()', 
+      function (newValue, oldValue) {
+        console.log("Old value: ", oldValue);
+        console.log("New value: ", newValue);
+
+        // if (newValue === true) {
+        //   displayCookieWarning();
+        // }
+        // else {
+        //   removeCookieWarning();
+        // }
+
+      }
+    );
+
+    // function displayCookieWarning() {
+    //   // Using Angluar jqLite
+    //   // var warningElem = element.find("div");
+    //   // console.log(warningElem);
+    //   // warningElem.css('display', 'block');
+
+    //   // If jQuery included before Angluar
+    //   var warningElem = element.find("div.error");
+    //   warningElem.slideDown(900);
+    // }
+
+
+    // function removeCookieWarning() {
+    //   // Using Angluar jqLite
+    //   // var warningElem = element.find("div");
+    //   // warningElem.css('display', 'none');
+
+    //   // If jQuery included before Angluar
+    //   var warningElem = element.find("div.error");
+    //   warningElem.slideUp(900);
+    // }
+  }
+
+  function FoundItemsDirectiveController() {
+    
+  }
+
+
+
   // Controller NarrowItDownController
   NarrowItDownController.$inject = ['$scope', 'MenuSearchService'];
-  
+
   function NarrowItDownController($scope, MenuSearchService) {
     $scope.found = [];
     
     $scope.teste = function() {
-      $scope.found = MenuSearchService.getMatchedMenuItems()
+      $scope.found = MenuSearchService.getMatchedMenuItems();
       console.log($scope.found)
     }
   }
